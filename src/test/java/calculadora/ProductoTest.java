c2e7f463cc805ea250b90b232e746e8fd44305e5
Laura Vinseiro <lauravinseirogutierrez@gmail.com>
2025-02-13 16:35:32 +0100

    Se añade estructura carpetas test

---
+++ src/test/java/calculadora/ProductoTest.java
@@ -0,0 +1,37 @@
+package calculadora;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void multiMenu() {
+    }
+
+    @Test
+    void prod2reales() {
+    }
+
+    @Test
+    void prod2enteros() {
+    }
+
+    @Test
+    void prod3reales() {
+    }
+
+    @Test
+    void potencia() {
+    }
+}
