8273ad2e82b58d2bc2ebdcf5285791c72134a823
alejandro lopez <alex24000000@gmail.com>
2025-02-16 22:40:03 +0100

    Agregado tratamiento de errores en Resta y arreglo de SumaTest

--- src/main/java/calculadora/Resta.java
+++ src/main/java/calculadora/Resta.java
@@ -1,5 +1,6 @@
 package calculadora;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
  * Clase que maneja operaciones de resta con diferentes tipos de números,
@@ -14,7 +15,7 @@ public class Resta {
     static Scanner sc = new Scanner(System.in);
     static float acumulado;
     static boolean primero = false;
-
+    static boolean flag=true;
     /**
      * Muestra un menú y maneja las diferentes opciones de resta elegidas por el usuario.
      * Permite restar 2 números reales, 2 números enteros, 3 números reales,
@@ -26,58 +27,104 @@ public class Resta {
         // Bucle infinito hasta que el usuario elija salir
         while (true) {
             System.out.println("Elija como deseas restar:");
-            System.out.println("\n1- Restar 2 números reales.");
-            System.out.println("2- Restar 2 números enteros.");
-            System.out.println("3- Restar 3 números reales");
+            System.out.println("\n1- Restar 2 numeros reales.");
+            System.out.println("2- Restar 2 numeros enteros.");
+            System.out.println("3- Restar 3 numeros reales");
             System.out.println("4- Restar al acumulado");
             System.out.println("\nIntroduzca 5 para salir.");
 
-            introducido = sc.nextInt(); // Leer la opción seleccionada
+            try {
+                introducido = sc.nextInt(); // Leer la opción seleccionada
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debes ingresar un numero entero.");
+                sc.nextLine();  // Limpiar el buffer de entrada
+                continue;  // Volver a pedir el menú
+            }
 
             switch (introducido) {
                 case 1:
-                    System.out.println("Introduzca el valor al que se le va a restar.");
-                    float restareal1 = sc.nextFloat();
-                    System.out.println("Introduzca el valor restado.");
-                    float restareal2 = sc.nextFloat();
-                    System.out.println("Resultado: " + restaReal(restareal1, restareal2));
+                    while (true) {
+                        try {
+                            System.out.println("Introduzca el valor al que se le va a restar.");
+                            float restareal1 = sc.nextFloat();
+                            System.out.println("Introduzca el valor restado.");
+                            float restareal2 = sc.nextFloat();
+                            System.out.println("Resultado: " + restaReal(restareal1, restareal2));
+                            break;  // Salir del bucle una vez que la entrada es válida
+                        } catch (InputMismatchException e) {
+                            System.out.println("Error: Debes ingresar numeros reales.");
+                            sc.nextLine(); // Limpiar el buffer de entrada
+                        }
+                    }
                     break;
 
                 case 2:
-                    System.out.println("Introduzca el valor al que se le va a restar.");
-                    int restarent1 = sc.nextInt();
-                    System.out.println("Introduzca el valor restado.");
-                    int restarent2 = sc.nextInt();
-                    System.out.println("Resultado: " + restaEntero(restarent1, restarent2));
+                    while (true) {
+                        try {
+                            System.out.println("Introduzca el valor al que se le va a restar.");
+                            int restarent1 = sc.nextInt();
+                            System.out.println("Introduzca el valor restado.");
+                            int restarent2 = sc.nextInt();
+                            System.out.println("Resultado: " + restaEntero(restarent1, restarent2));
+                            break;  // Salir del bucle una vez que la entrada es válida
+                        } catch (InputMismatchException e) {
+                            System.out.println("Error: Debes ingresar numeros enteros.");
+                            sc.nextLine();  // Limpiar el buffer de entrada
+                        }
+                    }
                     break;
 
                 case 3:
-                    System.out.println("Introduzca el valor al que se le va a restar.");
-                    float restatres1 = sc.nextFloat();
-                    System.out.println("Introduzca el primer valor restado.");
-                    float restatres2 = sc.nextFloat();
-                    System.out.println("Introduzca el segundo valor restado.");
-                    float restatres3 = sc.nextFloat();
-                    System.out.println("Resultado: " + restarTres(restatres1, restatres2, restatres3));
+                    while (true) {
+                        try {
+                            System.out.println("Introduzca el valor al que se le va a restar.");
+                            float restatres1 = sc.nextFloat();
+                            System.out.println("Introduzca el primer valor restado.");
+                            float restatres2 = sc.nextFloat();
+                            System.out.println("Introduzca el segundo valor restado.");
+                            float restatres3 = sc.nextFloat();
+                            System.out.println("Resultado: " + restarTres(restatres1, restatres2, restatres3));
+                            break;  // Salir del bucle una vez que la entrada es válida
+                        } catch (InputMismatchException e) {
+                            System.out.println("Error: Debes ingresar numeros reales.");
+                            sc.nextLine();  // Limpiar el buffer de entrada
+                        }
+                    }
                     break;
 
                 case 4:
                     if (!primero) {
-                        System.out.println("Introduzca el valor al que se le va a restar.");
-                        float restar = sc.nextFloat();
-                        setAcumulado(restar);
-                        primero = true;
-                        System.out.println("Valor acumulado: " + getAcumulado());
+                        while (true) {
+                            try {
+                                System.out.println("Introduzca el valor al que se le va a restar.");
+                                float restar = sc.nextFloat();
+                                setAcumulado(restar);
+                                primero = true;
+                                System.out.println("Valor acumulado: " + getAcumulado());
+                                break;  // Salir del bucle una vez que la entrada es válida
+                            } catch (InputMismatchException e) {
+                                System.out.println("Error: Debes ingresar un numero real.");
+                                sc.nextLine();  // Limpiar el buffer de entrada
+                            }
+                        }
                     } else {
-                        System.out.println("Introduzca el valor que se va a restar.");
-                        float restar = sc.nextFloat();
-                        System.out.println("Nuevo valor acumulado: " + restaAcumulada(restar));
+                        while (true) {
+                            try {
+                                System.out.println("Introduzca el valor que se va a restar.");
+                                float restar = sc.nextFloat();
+                                System.out.println("Nuevo valor acumulado: " + restaAcumulada(restar));
+                                break;  // Salir del bucle una vez que la entrada es válida
+                            } catch (InputMismatchException e) {
+                                System.out.println("Error: Debes ingresar un numero real.");
+                                sc.nextLine();  // Limpiar el buffer de entrada
+                            }
+                        }
                     }
                     break;
 
                 case 5:
                     System.out.println("Saliendo...");
-                    return; // Salir del método ejecutarMenu
+                    return;
 
                 default:
                     System.out.println("Opción incorrecta.");
